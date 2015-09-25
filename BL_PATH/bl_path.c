67f6639a082bde5f817ee4f266b6f28aa06fdab3
Ben Louie <blouie@cs.nmt.edu>
2015-09-25 13:52:50 -0600

    starting basic 2D array pathfinding
    unfinished: there's a 2D array of nodes, the path is created as the x,y
    coordinates try to move to the end coordinates.

--- BL_PATH/bl_path.c
+++ BL_PATH/bl_path.c
@@ -2,3 +2,48 @@
 /*///////////////////*/
 
 #include "bl_path.h"
+
+struct bl_path_fieldnode_t {
+    char open; // open or not
+    char data1; // future data
+    char data2; // future data
+};
+
+struct bl_path_pathnode_t {
+    char visited;
+    char split; // notifies splits and merges in path
+    //int count; // the number of moves since the start to that node (for easier queue creation)
+};
+
+struct bl_path_sten_t {
+    int sx; //start x
+    int sy; //start y
+    int ex; //end x
+    int ey; //end y
+};
+
+
+
+// first try at pathfinding
+void bl_path_arr_basic(bl_part_fieldnode **field, int fieldx, int fieldy, bl_path_sten coord) // should return a list (queue?) of which nodes to take to the end
+{
+    int x = coord.sx;
+    int y = coord.sy;
+    int endx = coord.ex;
+    int endy = coord.ey;
+    int chx = 0;
+    int chy = 0;
+    
+    bl_path_pathnode **nodeField = NULL;
+    
+    nodeField = calloc(fieldx * fieldy, sizeof(bl_path_pathnode)); // might need to malloc each row/column individually
+
+    
+    while (x != endx && y != endy) {
+        chx = (endx - x) > 0 ? 1 : (endx - x) < 0 ? -1 : 0;
+        chy = (endy - y) > 0 ? 1 : (endy - y) < 0 ? -1 : 0;
+        
+        // check if next node is open, if yes set appropriate pathnode.visited bit and go there (loop)
+        // work on splits when the node is closed
+    }
+}
--- BL_PATH/bl_path.h
+++ BL_PATH/bl_path.h
@@ -5,7 +5,11 @@
 #define BL_PATH_H
 
 #include <stdio.h>
+#include <stdlib.h>
 
+typedef struct bl_path_fieldnode_t bl_path_fieldnode;
+typedef struct bl_path_pathnode_t bl_path_pathnode;
+typedef struct bl_path_sten_t bl_path_sten;
 
 
 #endif
