dacdc0cf2ef5c03fd5d201a3232ccb9abdcd7cb1
benjlouie <benjlouie@gmail.com>
2015-10-05 14:05:10 -0700

    finished angle visited_direction calculations
    properly calculated chx,chy, and visited_direction

--- BL_PATH/bl_path.c
+++ BL_PATH/bl_path.c
@@ -51,35 +51,52 @@ int get_chxy_direction(int x, int y, int endx, int endy, int *chx, int *chy)
     int dir[3][3] = {{5, 6, 7}, {4, -1, 0}, {3, 2, 1}};
     int rise, run;
     int visited_direction;
-    float angle;
+    float slope;
     
     // should only be one or the other here
     rise = (endy - y);
     run = (endx - x);
     if(run == 0) {
         if(rise > 0) {
-            angle = 3.0;
+            slope = 3.0;
         } else {
-            angle = -3.0;
+            slope = -3.0;
         }
     } else if(rise == 0) {
         if(run > 0) {
-            angle = 0.0;
+            slope = 0.0;
         } else {
-            angle = -0.0;
+            slope = -0.0;
         }
     } else {
-        angle = (float)rise / (float)run;
-        
-        //     angles    |  slope
-        // -22.5 -- 22.5 | < 0.414
-        // 22.5 -- 67.5 | 0.414 -- 2.414
-        // 67.5 -- 90.0 | > 2.414
-        
-        // from this get chx and chy from dir[]
-        
+        slope = (float)rise / (float)run;
     }
-
+    
+    //     angles    |  slope
+    // -22.5 -- 22.5 | < 0.414
+    // 22.5 -- 67.5 | 0.414 -- 2.414
+    // 67.5 -- 90.0 | > 2.414
+    // from this get chx and chy from dir[]
+    
+    if(slope < 0) {
+        slope *= -1;
+    }
+    
+    if(slope < 0.414) {
+        *chx = 1; *chy = 0;
+    } else if(slope > 2.414) {
+        *chx = 0; *chy = 1;
+    } else {
+        *chx = 1; *chy = 1;
+    }
+    
+    if(rise < 0) {
+        *chy *= -1;
+    }
+    if(run < 0) {
+        *chx *= -1;
+    }
+    
     //*chx = (endx - x) > 0 ? 1 : (endx - x) < 0 ? -1 : 0;
     //*chy = (endy - y) > 0 ? 1 : (endy - y) < 0 ? -1 : 0;
     
